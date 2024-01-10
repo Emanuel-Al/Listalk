59287ffb9e524a1165f6db41b566c2cd53cc3517
Emanuel_R <emanuelreino13@gmail.com>
2024-01-10 14:12:31 -0300

    feat: Header and header btn

---
+++ listalk/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+import Header from '../../components/Header/Header'
+
+export default function Home() {
+  return (
+    <main>
+      <Header showLoginButton = {true} showRegisterButton = {true}/>
+    </main>
+  )
+}
