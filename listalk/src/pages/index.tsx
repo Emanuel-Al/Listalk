070f6505c4a649100994d4713ceed1a369343cff
Emanuel_R <emanuelreino13@gmail.com>
2024-01-15 13:58:44 -0300

    feat: Home Page Created

--- listalk/src/pages/index.tsx
+++ listalk/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import styles from './page.module.css'
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/Header/Header'
+import HomeContent from '../../components/HomeContent/HomeContent'
 
 export default function Home() {
   return (
     <main>
       <Header showLoginButton = {true} showRegisterButton = {true}/>
-      <Footer/>
+      <HomeContent/>
+      <Footer/> 
     </main>
   )
 }
