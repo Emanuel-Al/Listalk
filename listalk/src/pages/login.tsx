ab42ed32c44665fb646a9ee3071e18aafe86913f
Emanuel_R <emanuelreino13@gmail.com>
2024-01-17 15:40:07 -0300

    Fix: Font and layout issues

--- listalk/src/pages/login.tsx
+++ listalk/src/pages/login.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import styles from "./login.module.css";
 import FormInput from "../../components/form/formInput";
-
+import HeaderBtn from "../../components/Header/HeaderBtn";
 const login = () => {
   return (
     <div>
       <Header showLoginButton={true} showRegisterButton={true} />
       <div className={styles.container}>
         <h1>Welcome Back !</h1>
-        <h4>Your organized world awaits</h4>
+        <p>Your organized world awaits</p>
       </div>
       <div className={styles.inputArea}>
         <FormInput
@@ -22,6 +22,7 @@ const login = () => {
           placeHolder="Insert password"
           inputType="password"
         />
+        <HeaderBtn buttonName="Login" buttonType="Login2"/>
       </div>
     </div>
   );
