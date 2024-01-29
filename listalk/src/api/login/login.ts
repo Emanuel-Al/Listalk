15b1e6f947bba6388d1e0748854075170cbfc1ba
Emanuel_R <emanuelreino13@gmail.com>
2024-01-29 13:57:44 -0300

    fix: footer container fixed

--- listalk/src/api/login/login.ts
+++ listalk/src/api/login/login.ts
@@ -4,8 +4,8 @@ import api from "../http-common-keycloack"
 export async function postLogin(userName:string, password:string){
     return await api.post("realms/lmts/protocol/openid-connect/token", {
         client_secret: "Zlz3mGMEuuWGXAlUSjUE72qI0xZsaTPk",
-        Username: userName,
-        Password: password,
+        username: userName,
+        uassword: password,
         client_id: "app_lmts",
         grant_type: "password"
     })
